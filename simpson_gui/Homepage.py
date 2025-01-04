b4caa7f17ed9a7c0a157e2fe492a94212bfefda6
Subhradip Paul <subhradip.paul.sp@gmail.com>
2025-01-04 18:10:39 +0100

    finished par section of the app and created pages

---
+++ simpson_gui/Homepage.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from st_pages import Page, Section, show_pages, add_indentation
+
+col1, col2, col3 = st.columns(3,vertical_alignment="bottom", gap="medium")
+with col1:
+    st.image('resources/dnp_grenoble_logo.png', use_container_width=True)
+with col2:
+    st.image('resources/python.png', use_container_width=True)
+with col3:
+    st.image('resources/streamlit.png', use_container_width=True)
+
+
+st.markdown('''**Welcome to the SIMPSON GUI**  
+
+This interface is designed to help generate SIMPSON simulation files. 
+
+*Helpful Features*
+
+
+- You can input distances for dipoles. 
+- Use an XYZ file for generating dipolar coupling. 
+- It provides functionality to visualize powder files.
+- Multi select method for propagators.
+
+Please note that the generated simulation files serve as guides. You are encouraged to modify them as needed to suit your requirements. If you encounter any errors, feel free to reach out. However, I cannot take responsibility for simulations not working as intended. For the best experience, consider following official courses or tutorials available on the [SIMPSON website]().
+### Helpful Publications:
+
+
+1. Bak, Mads, Jimmy T. Rasmussen, and Niels Chr Nielsen. ‘SIMPSON: A General Simulation Program for Solid-State NMR Spectroscopy’. Journal of Magnetic Resonance 147, no. 2 (December 2000): 296–330. https://doi.org/10.1006/jmre.2000.2179.
+2. Juhl, Dennis W., Zdeněk Tošner, and Thomas Vosegaard. ‘Versatile NMR Simulations Using SIMPSON’. In Annual Reports on NMR Spectroscopy, 100:1–59. Elsevier, 2020. https://doi.org/10.1016/bs.arnmr.2019.12.001.
+3. Tošner, Zdeněk, Rasmus Andersen, Baltzar Stevensson, Mattias Edén, Niels Chr Nielsen, and Thomas Vosegaard. ‘Computer-Intensive Simulation of Solid-State NMR Experiments Using SIMPSON’. Journal of Magnetic Resonance 246 (September 2014): 79–93. https://doi.org/10.1016/j.jmr.2014.07.002.
+4. Vosegaard, Thomas, Anders Malmendal, and Niels C Nielsen. ‘The Flexibility of SIMPSON and SIMMOL for Numerical Simulations in Solid-and Liquid-State NMR Spectroscopy’. Monatshefte f?R Chemie / Chemical Monthly 133, no. 12 (1 December 2002): 1555–74. https://doi.org/10.1007/s00706-002-0519-2.
+
+### Upcoming Features:
+The pulse sequence and main section will be added soon.
+### Additional Resources:
+You can explore more SIMPSON-related files in our GitHub repository:
+[SIMPSON GitHub Repository](https://github.com/dnp-grenoble/simpson)
+''')
---
+++ simpson_gui/pages/02_Simpson_Parameters.py
@@ -0,0 +1,280 @@
+#%% Header files
+import re
+import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+
+def zcw_sequence(n, zcw_type = 1) :
+    """
+    Generates ZCW sequence and computes orientation angles.
+    Takes N (number of orientations) directly as input.
+
+    Parameters:
+        n (int): Number of orientations (direct input).
+        zcw_type (float): Type parameter which modifies behavior (1, 0.5, or 0.25).
+
+    Returns:
+        tuple: (alpha, beta, gamma) NumPy arrays of angles.
+    """
+    # Match the `type` parameter
+    if zcw_type == 1 :
+        c = [ 1 , 2 , 1 ]
+    elif zcw_type == 0.5 :
+        c = [ -1 , 1 , 1 ]
+    elif zcw_type == 0.25 :
+        c = [ -1 , 1 , 4 ]
+    else :
+        raise ValueError ( "Unsupported ZCW type. Use 1, 0.5, or 0.25." )
+
+    # Use an approximate Fibonacci value for g2
+    # g2 is typically g(M-2), but we use N to calculate an estimate
+    g2 = int ( n / 2.618 )  # Use the golden ratio approximation for a balanced g2
+
+    # Initialize output arrays
+    beta = np.zeros ( n )
+    alpha = np.zeros ( n )
+    gamma = np.zeros ( n )  # Gamma is always 0.0
+
+    # Loop to calculate alpha, beta, and gamma
+    for m in range ( 1 , n + 1 ) :  # Loop from 1 to n (inclusive)
+        beta[ m - 1 ] = (180 / np.pi) * np.arccos ( c[ 0 ] * (c[ 1 ] * ((m / n) % 1.0) - 1.0) )
+        alpha[ m - 1 ] = (180 / np.pi) * 2.0 * np.pi * ((m * g2 / n) % 1.0) / c[ 2 ]
+        gamma[ m - 1 ] = 0.0  # Gamma is always 0 in the original logic
+
+    return alpha , beta
+
+
+def repulsion_sequence(n) :
+    num_orientations = np.loadtxt( '../resources/repangles_num.txt' , dtype=float )
+    alpha_angles = np.loadtxt( '../resources/repangles_alpha.txt' , dtype=float )
+    beta_angles = np.loadtxt ( '../resources/repangles_beta.txt' , dtype=float )
+    idx = np.where(num_orientations == n)[0]
+
+    alpha_rep = alpha_angles[:, idx].flatten()
+    beta_rep = beta_angles[:, idx].flatten()
+
+    alpha_rep = alpha_rep[ np.flatnonzero( alpha_rep ) ]
+    beta_rep = beta_rep[(np.flatnonzero(beta_rep))]
+
+    alpha_rep = np.where( alpha_rep < 0 , alpha_rep , 360.0 + alpha_rep )
+    beta_rep = np.where( beta_rep < 0 , beta_rep , 360.0 + beta_rep )
+
+    return alpha_rep, beta_rep
+
+
+
+
+
+def bcr_sequence(n) :
+    """
+    Generate BCR (Backus-Collins-Reinisch) sequence for powder averaging.
+
+    Parameters:
+        n (int): Number of orientations.
+
+    Returns:
+        np.ndarray: Array of angles (alpha, beta) in radians.
+    """
+    indices = np.arange ( 0 , n , dtype=float ) + 0.5
+    phi = 2 * np.pi * indices / n  # Uniform distribution of azimuthal angle
+    theta = np.arccos ( 1 - 2 * indices / n )  # Uniform distribution of polar angle
+    return phi*180./np.pi , theta * 180./np.pi
+
+
+
+
+
+def plot_points_on_sphere_with_plotly(theta , phi) :
+    """
+    Plot points on a sphere using Plotly based on theta and phi angles.
+    """
+    theta = theta * np.pi / 180.
+    phi = phi * np.pi / 180.
+    # Convert spherical coordinates to Cartesian
+    x = np.sin ( theta ) * np.cos ( phi )
+    y = np.sin ( theta ) * np.sin ( phi )
+    z = np.cos ( theta )
+
+    # Create sphere surface
+    u = np.linspace ( 0 , 2 * np.pi , 100 )  # Azimuthal angles
+    v = np.linspace ( 0 , np.pi , 100 )  # Polar angles
+    x_sphere = np.outer ( np.sin ( v ) , np.cos ( u ) )
+    y_sphere = np.outer ( np.sin ( v ) , np.sin ( u ) )
+    z_sphere = np.outer ( np.cos ( v ) , np.ones_like ( u ) )
+
+    # Create Plotly figure
+    fig = go.Figure ()
+
+    # Add the sphere surface
+    fig.add_trace ( go.Surface (
+        x=x_sphere ,
+        y=y_sphere ,
+        z=z_sphere ,
+        colorscale='Blues' ,
+        opacity=0.5 ,
+        showscale=False
+    ))
+    # Add the plane (let's say at z=0.5, define its size)
+    plane_z = 0.  # The Z-value where the plane will be placed
+    plane_x , plane_y = np.meshgrid ( np.linspace ( -1 , 1 , 100 ) , np.linspace ( -1 , 1 , 100 ) )  # Define a meshgrid
+    plane_z_values = np.full ( plane_x.shape , plane_z )  # All Z-values will have the value 0.5
+
+    # Add the plane to the figure
+    fig.add_trace ( go.Surface (
+        x=plane_x ,
+        y=plane_y ,
+        z=plane_z_values ,
+        colorscale='Reds' ,  # Give a different color to the plane
+        opacity=0.2 ,
+        showscale=False
+    ) )
+
+    # Add the plane (let's say at z=0.5, define its size)
+    plane_y = 0.  # The y-value where the plane will be placed
+    plane_x , plane_z = np.meshgrid ( np.linspace ( -1 , 1 , 100 ) , np.linspace ( -1 , 1 , 100 ) )  # Define a meshgrid
+    plane_y_values = np.full ( plane_x.shape , plane_y )  # All Z-values will have the value 0.5
+
+    # Add the plane to the figure
+    fig.add_trace ( go.Surface (
+        x=plane_x ,
+        z=plane_z ,
+        y=plane_y_values ,
+        colorscale='Reds' ,  # Give a different color to the plane
+        opacity=0.2 ,
+        showscale=False
+    ) )
+
+    # Add data points on the sphere
+    fig.add_trace ( go.Scatter3d (
+        x=x ,
+        y=y ,
+        z=z ,
+        mode='markers' ,
+        marker=dict ( size=3 , color='MediumPurple' ) ,
+        name='Points'
+    ) )
+
+
+
+    # Update layout
+    fig.update_layout ( scene=dict (
+        xaxis=dict ( visible=False ) ,
+        yaxis=dict ( visible=False ) ,
+        zaxis=dict ( visible=False ) ,
+        aspectmode='data'
+    ) )
+
+    # Show plot
+    st.plotly_chart ( fig )
+
+
+def main () :
+    option_of_field = st.selectbox ( "Field in MHz (1H) or T" ,
+                                     ("MHz" , "Tesla") ,
+                                     )
+    if option_of_field == "MHz" :
+        field = st.number_input ( "Enter the field in MHz" , value=400.0)
+        latex_expr = "\\times 10^6"
+        st.write(f"Proton Frequency = ${field}$ ${latex_expr}$ Hz")
+    else :
+        field = st.number_input ( "Enter the field in T" , value=9.4)
+        latex_expr = "\\times 10^6"
+        field = field * 42.58
+        st.write ( f"Proton Frequency = ${field}$ ${latex_expr}$ Hz" )
+
+    spinning_frequency = st.number_input ( "Spinning Frequency in Hz" , min_value=1 , max_value=500000 , value=10000 )
+
+    options_crystal_file = [ "LEBoct31" , "alpha0beta0" , "alpha0beta90" , "bcr10" ,
+                             "bcr100" , "bcr20" , "bcr200" , "bcr30" , "bcr40" , "bcr400" ,
+                             "bcr50" , "bcr80" , "rep10" , "rep100" , "rep144" , "rep168" ,
+                             "rep20" , "rep2000" , "rep256" , "rep30" , "rep320" , "rep66" ,
+                             "rep678" , "repoct41" , "zcw143" , "zcw20" , "zcw232" , "zcw33" ,
+                             "zcw376" , "zcw4180" , "zcw54" , "zcw615" , "zcw88" , "zcw986" ,
+                             "zcw28656" ]
+    powder_file = st.selectbox ( "Which powder averaging scheme?" , options_crystal_file ,
+                                 index=None )
+    if powder_file is not None :
+        match = re.split ( r"([A-Za-z]+)([0-9]+)" , powder_file )
+        if match[ 1 ] == 'bcr' :
+            alpha , beta = bcr_sequence ( int ( match[ 2 ] ) )
+            plot_points_on_sphere_with_plotly ( alpha , beta )
+        elif match[ 1 ] == 'zcw' :
+            alpha , beta = zcw_sequence ( int ( match[ 2 ] ) )
+            plot_points_on_sphere_with_plotly ( alpha , beta )
+        elif match[ 1 ] == 'rep' :
+            alpha , beta = repulsion_sequence ( float ( match[ 2 ] ) )
+            plot_points_on_sphere_with_plotly ( alpha , beta )
+        elif powder_file == "alpha0beta0":
+            plot_points_on_sphere_with_plotly ( np.zeros(100) , np.zeros(100))
+        elif powder_file == "alpha0beta90":
+            plot_points_on_sphere_with_plotly ( 90.0 * np.ones ( 100 ), np.zeros ( 100 )  )
+        else :
+            st.write ( 'Cannot generate figure'
+                       )
+
+    gamma_angles = st.number_input ("Number of gamma angles", min_value=0, max_value=512, value=8, format='%d')
+
+    start_operator = st.text_input("Start Operator, can be In{p} p = x, y, z, p, m, c", value = "I1x")
+
+    detect_operator = st.text_input("Detect Operator, can be In{p} p = x, y, z, p, m, c", value = "I1p")
+
+
+    verbose = st.number_input("Verbose for the simulation", format='%d', value=1101)
+    vb_text = '''
+    The verbose entry takes up to eight zeroes or ones instructing SIMPSON to provide output about  
+    1. The spin system
+    2. The simulation progress
+    3. Simulation information
+    4. Start and detect operators
+    5. Powder angles
+    6. The rf averaging profile
+    7. The acquisition block
+    8. The averaging file.
+    
+    
+    The above value 1101 asks SIMPSON to provide output about the spin system, the simulation progress, and the start- and detect operators
+    '''
+    st.markdown(vb_text)
+
+    number_of_acq_points = st.number_input("Number of points to be detected", value=16, format='%d')
+
+    sw = st.text_input("Spectral window", value="spin_rate")
+
+    conjugate_fid = st.checkbox("Conjugate FID?", value=False)
+
+    value_methods = ["direct", "freq", "gcompute", "dysev", "dysevr", "pade", "taylor", "cheby1", "cheby2"]
+    method_of_sim = st.pills("Method of propagation", value_methods, selection_mode="multi")
+
+    gen_str_method = ""
+    for methods in method_of_sim:
+        gen_str_method = gen_str_method + methods + " "
+
+    st.image('resources/prop_methods.jpg', width=256, caption="Performance comparison of different strategies implemented in "
+                                                              "SIMPSON for propagator calculation for one carbon and 1–9 protons"
+                                                              "Tošner, Z. ‘Computer-Intensive Simulation of Solid-State NMR Experiments Using SIMPSON’. JMR 246 (2014): 79–93. https://doi.org/10.1016/j.jmr.2014.07.002."
+             )
+
+
+
+    par_code = (" par {  \n "
+                "\t proton_frequency \t" + f"{field*1e6} \n"
+                "\t spinning_rate \t" + f"{spinning_frequency} \n"
+                "\t crystal_file \t" + f"{powder_file} \n"
+                "\t gamma_angles \t" + f"{gamma_angles} \n"
+                "\t start_operator \t" + f"{start_operator} \n"
+                "\t detect_operator \t" + f"{detect_operator} \n"
+                "\t verbose \t" + f"{verbose} \n"
+                "\t np \t" + f"{number_of_acq_points} \n"
+                "\t sw \t" + f"{sw} \n"
+                "\t conjugate_fid \t" + f"{conjugate_fid} \n"
+                "\t method \t" + f"{gen_str_method} \n"
+                "}"
+                )
+    st.divider()
+    if st.button("Generate code"):
+        st.code(par_code, language="Tcl")
+
+if __name__ == '__main__':
+    st.title("To generate par section of SIMPSON")
+    st.divider()
+    main()
